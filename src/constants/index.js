49f010a7cee3fd34802ae32f3f9902bbaa9cb49d
Shashkov Danil <shashkovdanil@gmail.com>
2023-08-06 20:16:34 +0100

    new infra

--- src/constants/index.js
+++
@@ -1,5 +0,0 @@
-export const LIKE = 'like'
-export const DISLIKE = 'dislike'
-export const DEFAULT_PHOTO = '/img/assets/default.jpg'
-export const SENDER_EMAIL = 'no-reply@shortstories.com'
-export const API_URL = 'http://localhost:8000/graphql'
--- src/resolvers/index.js
+++
@@ -1,5 +0,0 @@
-import userResolvers from './user'
-import storyResolvers from './story'
-import commentResolvers from './comment'
-
-export default [userResolvers, storyResolvers, commentResolvers]
