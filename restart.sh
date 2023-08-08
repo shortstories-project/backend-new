4a8959244f6c06c944b1cd373635dc0d24b09483
Shashkov Danil <shashkovdanil@gmail.com>
2023-08-08 05:55:04 +0100

    upd infra

--- restart.sh
+++ restart.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 git pull
-npm install
-npm run build
-pm2 restart api
+/root/.nvm/versions/node/v18.17.0/bin/node
+/root/.nvm/versions/node/v18.17.0/bin/npm install
+/root/.nvm/versions/node/v18.17.0/bin/npm run build
+/root/.nvm/versions/node/v18.17.0/bin/pm2 restart api
